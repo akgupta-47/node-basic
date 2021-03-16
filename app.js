119971eb48df2efe484e944418fffa00830c83f9
akgupta-47 <akshatgupta3214@gmail.com>
2021-03-17 01:22:35 +0530

    eslint and prettier added

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
-
+const morgan = require('morgan');
 const app = express();
 const basicRouter = require('./routes/basicRoute');
 app.use(express.json());
+app.use(morgan('dev'));
 
 app.use('/',basicRouter);       
 
